3281b61bdfce11722f86d48b378b31b1a036caa3
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2018-01-27 16:52:08 +0330

    new Entitiy

--- src/main/java/com/example/StudentRepository.java
+++ src/main/java/com/example/StudentRepository.java
@@ -7,6 +7,7 @@ import java.util.List;
 public interface StudentRepository extends MongoRepository<Student, String> {
 
     Customer findByFirstName(String firstName);
+
     List<Student> findByLastName(String lastName);
 
 }
